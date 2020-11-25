d1d84cf13e83b8e82a94a5e4a1714ebeb8887d49
brayokenya <kiirubrian21.com>
2020-11-26 01:45:56 +0300

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// used to ease import clustering on app.js
+
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Charts/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
